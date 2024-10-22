d6d01aba4bf4e7ba10053b039d2a6914ef672873
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-10-23 00:19:10 +0300

    refactor: change Nodes' coordinates validation method to more general one method

--- src/main/java/backend/academy/maze/AStarPath.java
+++ src/main/java/backend/academy/maze/AStarPath.java
@@ -26,14 +26,14 @@ public class AStarPath implements Solver {
     public List<Node> solve() {
         int xStep = 0;
         int yStep = 1;
-        if (!isValidInitPos()) {
+        if (!isValid(start.row(), start.col())) {
             return shortestPath;
         }
-        PriorityQueue<Node> futureSteps = new PriorityQueue<>();
-        futureSteps.add(new Node(start.row(), start.col(), 0, heuristic(0, 0), null));
+        PriorityQueue<Node> priorityTraverse = new PriorityQueue<>();
+        priorityTraverse.add(new Node(start.row(), start.col(), 0, heuristic(0, 0), null));
         Node current;
-        while (!futureSteps.isEmpty()) {
-            current = futureSteps.poll();
+        while (!priorityTraverse.isEmpty()) {
+            current = priorityTraverse.poll();
             if (current.x == end.row() && current.y == end.col()) {
                 while (current != null) {
                     shortestPath.add(current);
@@ -44,29 +44,19 @@ public class AStarPath implements Solver {
             for (int[] nextStep : DIRECTIONS) {
                 int newX = current.x + nextStep[xStep];
                 int newY = current.y + nextStep[yStep];
-                if (destinations[newX][newY] == null && isValid(newX, newY)) {
+                if (isValid(newX, newY)) {
                     Node neighborNode = new Node(newX, newY, current.g + 1, heuristic(newX, newY), current);
                     destinations[newX][newY] = neighborNode;
-                    futureSteps.add(neighborNode);
+                    priorityTraverse.add(neighborNode);
                 }
             }
         }
         return shortestPath;
     }
 
-    private boolean isValidInitPos() {
-        return start.row() == 0 && start.col() == 1
-            || start.row() == maze.height() - 2 && start.col() == maze.width() - 1
-            || start.row() > 0 && start.row() < maze.height() - 1 && start.col() > 0 && start.col() < maze.height() - 1
-            && (end.row() == 0 && end.col() == 1
-            || end.row() == maze.height() - 2 && end.col() == maze.width() - 1
-            || end.row() > 0 && end.row() < maze.height() - 1 && end.col() > 0 && end.col() < maze.height() - 1);
-
-    }
-
     private boolean isValid(int x, int y) {
         return x >= 0 && x < maze.height() && y >= 0 && y < maze.width()
-            && maze.grid()[x][y].type() == Cell.Type.PASSAGE;
+            && maze.grid()[x][y].type() != Cell.Type.WALL && destinations[x][y] == null;
     }
 
     private int heuristic(int x, int y) {
