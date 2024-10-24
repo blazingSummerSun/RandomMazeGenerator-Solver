c9167ebbb4e59d748a1e349921b01923de646c5d
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-10-24 18:45:10 +0300

    feat: add launch maze generation from main class

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,21 @@
 package backend.academy;
 
+import backend.academy.maze.Maze;
+import backend.academy.maze.MazeAsSymbolsRenderer;
+import backend.academy.maze.MazeSelector;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        MazeSelector mazeSelector = new MazeSelector(System.in, System.out);
+        mazeSelector.algorithmSelection();
+        mazeSelector.sizeSelection();
+        Maze maze = mazeSelector.mazeGeneration();
+        MazeAsSymbolsRenderer renderer = new MazeAsSymbolsRenderer();
+        renderer.render(maze, System.out);
+        mazeSelector.getPathCoordinates();
+        mazeSelector.getMazeSolver();
+        renderer.render(maze, mazeSelector.applySolver(), System.out);
     }
 }
