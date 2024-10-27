4bfa0d1b346996a378b9dd99a14b5eb927ea8f50
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-10-27 20:54:14 +0300

    refactor: factor out specific heuristic for different type of cells

--- src/main/java/backend/academy/maze/Cell.java
+++ src/main/java/backend/academy/maze/Cell.java
@@ -1,7 +1,19 @@
 package backend.academy.maze;
 
 public record Cell(int row, int col, Type type) {
+    private final static int SWAMP_COST = 5;
+    private final static int PASSAGE_COST = 5;
+    private final static int LAKE_COST = 5;
+
     public enum Type {
-        WALL, PASSAGE, ANSWER, SWAMP
+        WALL, PASSAGE, ANSWER, SWAMP, LAKE
+    }
+
+    public int cellCost() {
+        return switch (type) {
+            case SWAMP -> SWAMP_COST;
+            case LAKE -> LAKE_COST;
+            default -> PASSAGE_COST;
+        };
     }
 }
